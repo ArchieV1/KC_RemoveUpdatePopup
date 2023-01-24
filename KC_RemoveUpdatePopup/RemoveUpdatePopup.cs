32db9705012b8e397362d5d8d7c2a47d3b6c8559
ArchieV1 <archie.j.vann@gmail.com>
2023-01-24 17:48:36 +0000

    Init

---
+++ KC_RemoveUpdatePopup/RemoveUpdatePopup.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace KC_RemoveUpdatePopup
+{
+    public class RemoveUpdatePopup : MonoBehaviour
+    {
+        public void Preload(KCModHelper helper)
+        {
+            
+        }
+        
+        public void Start()
+        {
+            // Delete update news
+            Destroy(GameObject.Find("UpdateContainer"));
+            Destroy(GameObject.Find("UpdateDescription"));
+        }
+    }
+}
